b34958e97577c75ea397a357d3f4eb655b36498b
Martin Mendez <martinpercu@gmail.com>
2023-04-13 15:42:38 +0100

    just method GET in main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 app.title = "An app with FastAPI"
 app.version = "0.1"
 
+films = [
+    {
+        "id": 1,
+		"title": "Avatars",
+		"overview": "Un human va a la planete pandora ou tous sont bleus ....",
+		"year": "2012",
+		"rating": 7.8,
+		"category": "Action"
+    }
+]
+
 @app.get('/', tags=['Home'])
 def message():
-    return "Bonjour monde"
+    return HTMLResponse('<h2>"Bonjour monde"</h2>')
+
+@app.get('/films', tags=['Films'])
+def get_films():
+    return films
+
