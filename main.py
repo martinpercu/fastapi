1228395e27f68d5a244ae85ac3951709205df4d6
Martin Mendez <martinpercu@gmail.com>
2023-04-13 15:50:22 +0100

    get by parameters

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ app.version = "0.1"
 films = [
     {
         "id": 1,
-		"title": "Avatars",
+		"title": "Avatar",
 		"overview": "Un human va a la planete pandora ou tous sont bleus ....",
 		"year": "2012",
 		"rating": 7.8,
+		"category": "Action"
+    },
+    {
+        "id": 2,
+		"title": "Avatar 2",
+		"overview": "Un human va a la planete pandora ou tous sont bleus ....",
+		"year": "2019",
+		"rating": 7.2,
 		"category": "Action"
     }
 ]
@@ -24,4 +32,12 @@ def message():
 @app.get('/films', tags=['Films'])
 def get_films():
     return films
+
+@app.get('/film/{id}', tags=['Films'])
+def get_film(id: int):
+    for item in films:
+        if item['id'] == id:
+            return item
+    return "Film not find"
+
 
